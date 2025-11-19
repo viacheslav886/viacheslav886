2b6be5f55dc76d6f528a3bcb08289fa1853a5caa
viacheslav886 <rubscr1337@gmail.com>
2025-11-19 07:56:26 +0500

    Add main program for SnakeAI game

---
+++ SnakeAI/Program.cs
@@ -0,0 +1,11 @@
+namespace SnakeAI
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Game game = new Game(25, 15);
+            game.Run();
+        }
+    }
+}
