b6bc357e64fa49611d3351eceefaf951c08894a4
viacheslav886 <rubscr1337@gmail.com>
2025-11-29 09:10:14 +0500

    Add Mp3Metadata class for storing MP3 metadata

---
+++ Simple MP3 Metadata Reader/Mp3MetaReader/Mp3MetaData.cs
@@ -0,0 +1,11 @@
+namespace Mp3MetaReader
+{
+    public class Mp3Metadata
+    {
+        public string Title { get; set; } = "";
+        public string Artist { get; set; } = "";
+        public string Album { get; set; } = "";
+        public string Year { get; set; } = "";
+        public string Genre { get; set; } = "";
+    }
+}
